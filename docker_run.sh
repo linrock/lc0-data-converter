ffc870401571df2d8e9d557b9d33c132d2cd86a5
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-25 12:44:57 -0500

    Scripts to build and run container

---
+++ docker_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Run the container in the background (detached)
+docker_container_id=$(docker run -d \
+  --gpus all \
+  --mount type=bind,source="$(pwd)"/../lc0-data,target=/root/lc0-data \
+  --mount type=bind,source="$(pwd)"/../syzygy,target=/root/syzygy \
+  lc0-rescorer)
+
+# Attach a bash shell to the running container
+docker exec -it $docker_container_id bash
