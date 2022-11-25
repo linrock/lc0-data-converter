9cccbd5d1ef9d2a4359384598aa3655ef9e869cb
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-25 22:09:49 +0000

    Enable downloading multiple data files in parallel

--- data_downloader.sh
+++ data_downloader.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-for filename in training-run1-test80-202211{18..24}-{00..23}17
+tmpfile=$(mktemp /tmp/lc0-data-urls.XXXXXX)
+for filename in training-run1-test80-202211{20..24}-{00..23}17
 do
-  wget https://storage.lczero.org/files/training_data/test80/$filename.tar
+  echo $filename >> $tmpfile
 done
+
+cat $tmpfile | parallel -j 1 \
+  wget https://storage.lczero.org/files/training_data/test80/{}.tar \
+    -O ~/lc0-data/{}.tar
+
+rm $tmpfile
