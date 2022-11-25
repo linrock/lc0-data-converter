336a471b3e14e96187c4f3ed1a4540d30358dda7
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-25 21:45:57 +0000

    Use 7p tablebases when available

--- run_rescorer.sh
+++ run_rescorer.sh
@@ -3,6 +3,6 @@
 data_dir=training-run1-test80-20221114-0017
 rescorer rescore \
   --input=lc0-data/$data_dir/ \
-  --syzygy-paths=./syzygy/345m:./syzygy/6m \
+  --syzygy-paths=./syzygy/345p:./syzygy/6p:./syzygy/7p \
   --nnue-plain-file=./output-data/$data_dir.plain \
   --threads=20
