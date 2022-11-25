9e750faee9ef8b34a6410dd6210a7cc427279550
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-25 19:53:51 +0000

    Output .plain file with same prefix as converted data dir

--- run_rescorer.sh
+++ run_rescorer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-./rescorer rescore \
-  --input=lc0-data/training-run1-test80-20221114-0017/ \
+data_dir=training-run1-test80-20221114-0017
+rescorer rescore \
+  --input=lc0-data/$data_dir/ \
   --syzygy-paths=./syzygy/345m:./syzygy/6m \
-  --nnue-plain-file=test.plain \
+  --nnue-plain-file=./output-data/$data_dir.plain \
   --threads=20
