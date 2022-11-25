e62df6be2f7de042e3a14f750a817e4846443444
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-25 22:30:12 +0000

    Enable 8 concurrent downloads

--- data_downloader.sh
+++ data_downloader.sh
@@ -6,8 +6,10 @@ do
   echo $filename >> $tmpfile
 done
 
-cat $tmpfile | parallel -j 1 \
-  wget https://storage.lczero.org/files/training_data/test80/{}.tar \
-    -O ~/lc0-data/{}.tar
+cat $tmpfile | xargs -P8 -I{} \
+   bash -c "
+     echo Downloading https://storage.lczero.org/files/training_data/test80/{}.tar && \
+     wget -q https://storage.lczero.org/files/training_data/test80/{}.tar \
+       -O ~/lc0-data/{}.tar"
 
 rm $tmpfile
