91ef9d85a619660cb70a18ee6534e9ceb543dd35
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-25 19:55:25 +0000

    Bind mount an output data directory

--- docker_run.sh
+++ docker_run.sh
@@ -5,6 +5,7 @@ docker_container_id=$(docker run -d \
   --gpus all \
   --mount type=bind,source="$(pwd)"/../lc0-data,target=/root/lc0-data \
   --mount type=bind,source="$(pwd)"/../syzygy,target=/root/syzygy \
+  --mount type=bind,source="$(pwd)"/../output-data,target=/root/output-data \
   lc0-rescorer)
 
 # Attach a bash shell to the running container
