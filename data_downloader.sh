c4293e77f8ec471792110f7355526da8712642ed
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-26 01:41:22 +0000

    Print if download fails. Over 10 concurrency = 429 too many requests

--- data_downloader.sh
+++ data_downloader.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+url_prefix="https://storage.lczero.org/files/training_data/test78"
+
 tmpfile=$(mktemp /tmp/lc0-data-urls.XXXXXX)
-for filename in training-run1-test80-202211{14..19}-{00..23}17
+for filename in training-run3-test78-202210{11..20}-{00..23}22
 do
   echo $filename >> $tmpfile
 done
 
 cat $tmpfile | xargs -P10 -I{} \
    bash -c "
-     echo Downloading https://storage.lczero.org/files/training_data/test80/{}.tar && \
-     wget -q https://storage.lczero.org/files/training_data/test80/{}.tar \
-       -O ~/lc0-data/{}.tar"
+     echo Downloading {}.tar && \
+       wget -q $url_prefix/{}.tar -O ~/lc0-test78/{}.tar || \
+       echo Failed to download {}.tar"
 
 rm $tmpfile
