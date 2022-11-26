f51a03e0f576fe824e67314199fb565f55abd830
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-26 16:26:30 -0500

    Rewrite run_rescorer.sh in a parallelizable way

--- run_rescorer.sh
+++ run_rescorer.sh
@@ -2,20 +2,25 @@
 # Takes lc0 .tar data files, rescores them with tablebases,
 # and converts them to .plain for stockfish to process later
 
-data_dir=./test80-data
+data_dir=./data/test80/may2022
 
-for filepath in $data_dir/*.tar; do
+function rescore_tar_file() {
+  filepath=$1
   tarfile=$(basename $filepath)
   dirname=$(basename $tarfile .tar)
 
   echo Extracting $tarfile ...
-  tar xvf $filepath -C $data_dir > /dev/null
+  tar xvf $filepath > /dev/null
 
   rescorer rescore \
-    --input=$data_dir/$dirname \
-    --syzygy-paths=./syzygy/345p:./syzygy/6p:./syzygy/7p \
-    --nnue-plain-file=./output-data/$dirname.plain \
+    --input=./$dirname \
+    --syzygy-paths=/root/syzygy/345p:/root/syzygy/6p:/root/syzygy/7p \
+    --nnue-plain-file=/root/data/$dirname.plain \
     --threads=20
 
-  rm -rf $data_dir/$dirname
-done
+  rm -rf $dirname
+}
+export -f rescore_tar_file
+
+cd $data_dir
+ls -1 *.tar | xargs -P3 -I{} bash -c 'rescore_tar_file "$@"' _ {}
