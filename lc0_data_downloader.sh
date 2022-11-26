76f5ba6ec34cd799d0899763a216cb78489b674e
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-26 00:15:50 -0500

    Add some comments to lc0 data downloader

--- lc0_data_downloader.sh
+++ lc0_data_downloader.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
+# Naive downloader for lc0 training data
+ 
 url_prefix="https://storage.lczero.org/files/training_data/test78"
+download_dir="~/lc0-test78"
 
 tmpfile=$(mktemp /tmp/lc0-data-urls.XXXXXX)
 for filename in training-run3-test78-202209{15..30}-{00..23}{21,22}
@@ -8,9 +10,11 @@ do
   echo $filename >> $tmpfile
 done
 
+# max concurrency 10, otherwise 429 errors
 xargs -a $tmpfile -P10 -I{} \
   bash -c "echo Downloading {}.tar && \
-    wget -q $url_prefix/{}.tar -O ~/lc0-test78/{}.tar || \
+    wget -q $url_prefix/{}.tar -O $download_dir/{}.tar || \
     echo Failed to download {}.tar"
 
+find $download_dir -size 0 -delete
 rm $tmpfile
