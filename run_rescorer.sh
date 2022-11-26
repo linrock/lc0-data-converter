d266d34374f62f8abfc402dc31283dda58c1bf8a
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-26 00:08:28 -0500

    Define data_dir in one place in run_rescorer.sh

--- run_rescorer.sh
+++ run_rescorer.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 # Takes lc0 .tar data files, rescores them with tablebases,
 # and converts them to .plain for stockfish to process later
-# Run this inside the container
 
-for filepath in ./test80-data/*.tar; do
+data_dir=./test80-data
+
+for filepath in $data_dir/*.tar; do
   tarfile=$(basename $filepath)
   dirname=$(basename $tarfile .tar)
 
   echo Extracting $tarfile ...
-  tar xvf $filepath -C ./test80-data > /dev/null
+  tar xvf $filepath -C $data_dir > /dev/null
 
   rescorer rescore \
-    --input=./test80-data/$dirname \
+    --input=$data_dir/$dirname \
     --syzygy-paths=./syzygy/345p:./syzygy/6p:./syzygy/7p \
     --nnue-plain-file=./output-data/$dirname.plain \
     --threads=20
 
-  rm -rf ./test80-data/$dirname
+  rm -rf $data_dir/$dirname
 done
