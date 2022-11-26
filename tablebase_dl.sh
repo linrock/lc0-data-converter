13ae7b81ed14a575f6cf97f625a96443ff8caebc
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-26 11:29:10 -0500

    Define tb7 source url, add list of additional 4v3

--- tablebase_dl.sh
+++ tablebase_dl.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-common_5v2_7p=(
-  KBPPPvKB
-)
+TB_URL="https://tablebase.lichess.ovh/tables/standard/7/"
+
+# practical 7-piece endgames:
+# https://groups.google.com/g/fishcooking/c/chP0S4jXTxU
+common_5v2_7p=(KBPPPvKB)
 common_4v3_7p=(
   KBPPvKBP
   KBPPvKNP
@@ -13,17 +15,31 @@ common_4v3_7p=(
   KQPPvKQP
   KRPPvKRP
 )
+
+# 2-pawn endgames with material difference <= 1
+eg_4v3_7p=(
+  KQPPvKRR
+  KNPPvKNP
+  KPPPvKPP
+  KBPPvKBP
+  KNPPvKBP
+  KRPPvKNN
+  KBPPvKNN
+  KRPPvKBB
+  KRPPvKBN
+  KRPPvKRP
+  KBPPvKRP
+  KNPPvKRP
+  KQPPvKQP
+)
+
 for pieces in ${common_5v2_7p[@]}; do
-  wget https://tablebase.lichess.ovh/tables/standard/7/5v2_pawnful/$pieces.rtbw \
-    -O ../syzygy/7p/$pieces.rtbw
-  wget https://tablebase.lichess.ovh/tables/standard/7/5v2_pawnful/$pieces.rtbz \
-    -O ../syzygy/7p/$pieces.rtbz
+  wget $TB_URL/5v2_pawnful/$pieces.rtbw -O ../syzygy/7p/$pieces.rtbw
+  wget $TB_URL/5v2_pawnful/$pieces.rtbz -O ../syzygy/7p/$pieces.rtbz
   echo $pieces
 done
 for pieces in ${common_4v3_7p[@]}; do
-  wget https://tablebase.lichess.ovh/tables/standard/7/4v3_pawnful/$pieces.rtbw \
-    -O ../syzygy/7p/$pieces.rtbw
-  wget https://tablebase.lichess.ovh/tables/standard/7/4v3_pawnful/$pieces.rtbz \
-    -O ../syzygy/7p/$pieces.rtbz
+  wget $TB_URL/4v3_pawnful/$pieces.rtbw -O ../syzygy/7p/$pieces.rtbw
+  wget $TB_URL/4v3_pawnful/$pieces.rtbz -O ../syzygy/7p/$pieces.rtbz
   echo $pieces
 done
