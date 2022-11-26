09b2becfcdecee136611ff84fa2053b262a57c7f
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-26 21:25:25 +0000

    Rename to lc0-converter in docker scripts

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-DOCKER_BUILDKIT=1 docker build -t lc0-rescorer .
+DOCKER_BUILDKIT=1 docker build -t lc0-converter .
--- docker_run.sh
+++ docker_run.sh
@@ -2,11 +2,9 @@
 
 # Run the container in the background (detached)
 docker_container_id=$(docker run -d \
-  --gpus all \
-  --mount type=bind,source="$(pwd)"/../lc0-data,target=/root/lc0-data \
+  --mount type=bind,source="$(pwd)"/../data,target=/root/data \
   --mount type=bind,source="$(pwd)"/../syzygy,target=/root/syzygy \
-  --mount type=bind,source="$(pwd)"/../output-data,target=/root/output-data \
-  lc0-rescorer)
+  lc0-converter)
 
 # Attach a bash shell to the running container
 docker exec -it $docker_container_id bash
