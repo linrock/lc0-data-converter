f544346967140c1c79644966449dee20c97e8d8d
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-26 02:45:53 +0000

    Use input arg for xargs instead of cat

--- data_downloader.sh
+++ data_downloader.sh
@@ -3,15 +3,14 @@
 url_prefix="https://storage.lczero.org/files/training_data/test78"
 
 tmpfile=$(mktemp /tmp/lc0-data-urls.XXXXXX)
-for filename in training-run3-test78-202210{11..20}-{00..23}22
+for filename in training-run3-test78-202209{15..30}-{00..23}{21,22}
 do
   echo $filename >> $tmpfile
 done
 
-cat $tmpfile | xargs -P10 -I{} \
-   bash -c "
-     echo Downloading {}.tar && \
-       wget -q $url_prefix/{}.tar -O ~/lc0-test78/{}.tar || \
-       echo Failed to download {}.tar"
+xargs -a $tmpfile -P10 -I{} \
+  bash -c "echo Downloading {}.tar && \
+    wget -q $url_prefix/{}.tar -O ~/lc0-test78/{}.tar || \
+    echo Failed to download {}.tar"
 
 rm $tmpfile
