e10106dff21dac35ad8ea9295b19ba1c975a4d47
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-26 04:09:50 +0000

    Allow specifying filenames for filter_no_castling.py

--- filter_no_castling.py
+++ filter_no_castling.py
@@ -1,8 +1,18 @@
+import sys
+
+if len(sys.argv) != 3:
+    print('Usage: ./filter_no_castling.py <input.plain> <output.plain>')
+    sys.exit(0)
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
 position = None
 num_positions = 0
 num_no_castling_positions = 0
-with open('out.plain', 'w+') as fout:
-    with open('test.plain', 'r') as fin:
+
+with open(output_file, 'w+') as fout:
+    with open(input_file, 'r') as fin:
         for row in fin:
             if 'fen' in row:
                 position = row
