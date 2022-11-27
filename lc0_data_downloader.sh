ecb92aacde77c1e940a5260ddbdeba3107de1d21
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-26 19:13:30 -0500

    Remove need for tempfile in lc0 data downloader

--- lc0_data_downloader.sh
+++ lc0_data_downloader.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 # Naive downloader for lc0 training data
- 
-url_prefix="https://storage.lczero.org/files/training_data/test78"
-download_dir="~/lc0-test78"
 
-tmpfile=$(mktemp /tmp/lc0-data-urls.XXXXXX)
-for filename in training-run3-test78-202209{15..30}-{00..23}{21,22}
-do
-  echo $filename >> $tmpfile
-done
+download_dir="~/data/test80/nov2022"
+url_prefix="https://storage.lczero.org/files/training_data/test80"
 
 # max concurrency 10, otherwise 429 errors
-xargs -a $tmpfile -P10 -I{} \
+xargs -P10 -a <(
+  for filename in training-run1-test80-202211{01..31}-{00..23}17
+  do
+    echo $filename
+  done
+) -I{} \
   bash -c "echo Downloading {}.tar && \
     wget -q $url_prefix/{}.tar -O $download_dir/{}.tar || \
     echo Failed to download {}.tar"
 
+# clean up files from download errors (ie. files that don't exist)
 find $download_dir -size 0 -delete
-rm $tmpfile
