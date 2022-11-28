8e4795a872a87a7c7d9f3ed55b0ec76b7421a518
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-27 23:25:00 -0500

    Simplify script a bit more

--- tb7/tablebase_dl.sh
+++ tb7/tablebase_dl.sh
@@ -5,21 +5,7 @@ dl_dir2="~/data/syzygy/7p"
 
 TB_URL="https://tablebase.lichess.ovh/tables/standard/7/"
 
-# practical 7-piece endgames:
-# https://groups.google.com/g/fishcooking/c/chP0S4jXTxU
-common_5v2_7p=(KBPPPvKB)
-common_4v3_7p=(
-  KBPPvKBP
-  KBPPvKNP
-  KNPPvKBP
-  KNPPvKNP
-  KPPPvKBP
-  KPPPvKPP
-  KQPPvKQP
-  KRPPvKRP
-)
-
-eg_4v3_7p=(
+tablebases_to_download=(
 KBBBvKQP
 KBBBvKRR
 KBBNvKQP
@@ -123,30 +109,30 @@ KRRPvKRR
 KRRRvKQR
 )
 
-function download_tb_data() {
-  pieces = $1  # KRBPvKQP, etc.
+function download_tb7_data() {
+  pieces=$1  # KRBPvKQP, etc.
   for suffix in {rtbw,rtbz}; do
     tb_filename=$pieces.$suffix
-    if [ ! -f $dl_dir1/$tb_filename ] && \
-       [ ! -f $dl_dir2/$tb_filename ]; then
-      if echo $pieces | grep -E 'K[QRBN]{4}v'; then
-        subtype=5v2
-      else if echo $pieces | grep -E 'K[QRBN]{3}v'; then
-        subtype=4v3
-      else
-        subtype=6v1
-      fi
-      if echo $pieces | grep P; then
-        subtype=${subtype}_pawnful
-      else
-        subtype=${subtype}_pawnless
-      fi
-      echo $tb_filename not found. Downloading ${subtype} ...
-      wget $TB_URL/$tb_filename -O $dl_dir1/$tb_filename
+    if [ -f $dl_dir1/$tb_filename ] || [ -f $dl_dir2/$tb_filename ]; then
+      continue
     fi
+    if echo $pieces | grep -E 'vK$' > /dev/null; then
+      subtype=6v1
+    elif echo $pieces | grep -E 'K[QRBN]{4}v' > /dev/null; then
+      subtype=5v2
+    else
+      subtype=4v3
+    fi
+    if echo $pieces | grep P > /dev/null; then
+      subtype=${subtype}_pawnful
+    else
+      subtype=${subtype}_pawnless
+    fi
+    echo $tb_filename not found. Downloading ${subtype} ...
+    # wget $TB_URL/$tb_filename -O $dl_dir1/$tb_filename
   done
 }
 
-for pieces in ${eg_4v3_7p[@]}; do
-  download_tb_data $pieces
+for pieces in ${tablebases_to_download[@]}; do
+  download_tb7_data $pieces
 done
