ab9aad8a4bc15392b8be8e9d2b778b842aeea67e
Linmiao Xu <linmiao.xu@gmail.com>
2022-12-15 22:44:37 -0500

    Remove empty tar files if downloads fail

--- lc0_data_downloader.sh
+++ lc0_data_downloader.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # Naive downloader for lc0 training data
 
-download_dir="~/data/test80-sep2022"
-url_prefix="https://storage.lczero.org/files/training_data/test80"
+DATA_URL_PREFIX="https://storage.lczero.org/files/training_data/test80"
+
+# where data is downloaded and processed
+download_dir=~/data/test80-sep2022
+cd $download_dir
 
 # max concurrency 10, otherwise http 429 errors
 xargs -P10 -a <(
@@ -11,10 +14,10 @@ xargs -P10 -a <(
     echo $filename
   done
 ) -I{} \
-  bash -c "ls -lth $download_dir/{}.binpack || ls -lth $download_dir/{}.tar || \
+  bash -c "ls -lth {}.binpack || ls -lth {}.tar || \
     (echo Downloading {}.tar ... && \
-     wget -q $url_prefix/{}.tar -O $download_dir/{}.tar || \
-     echo Failed to download {}.tar)"
+     wget -q $DATA_URL_PREFIX/{}.tar || \
+     (echo Failed to download {}.tar ; rm {}.tar ))"
 
 # clean up files from download errors (ie. files that don't exist)
 find $download_dir -name "*.tar" -size 0 -delete
