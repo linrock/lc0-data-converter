01c7ad23891279b27afe222be4ad7332f67fd371
Linmiao Xu <linmiao.xu@gmail.com>
2022-12-18 01:01:39 -0500

    Fetch list of valid tar files instead of brute forcing

--- lc0_data_downloader.sh
+++ lc0_data_downloader.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 # Naive downloader for lc0 training data
 
-DATA_URL_PREFIX="https://storage.lczero.org/files/training_data/test80"
-
-# where data is downloaded and processed
-download_dir=~/data/test80-sep2022
-cd $download_dir
+LC0_DATASET=test77
 
 # max concurrency 10, otherwise http 429 errors
+DATASET_URL="https://storage.lczero.org/files/training_data/$LC0_DATASET"
+
+# where data is downloaded and processed
+download_dir=~/data/$lc0_dataset
+mkdir -p $download_dir
+cd $download_dir
+
 xargs -P10 -a <(
-  for filename in training-run1-test80-202209{01..31}-{00..23}17
-  do
-    echo $filename
-  done
-) -I{} \
-  bash -c "ls -lth {}.binpack || ls -lth {}.tar || \
-    (echo Downloading {}.tar ... && \
-     wget -q $DATA_URL_PREFIX/{}.tar || \
-     (echo Failed to download {}.tar ; rm {}.tar ))"
+  #  for filename in training-run1-test80-202209{01..31}-{00..23}17
+  #  do
+  #    echo $filename
+  #  done
+  curl -sL $DATASET_URL | grep -oE "training-run[^>]*tar" | sort -u | xargs -I{} basename {} .tar
+) \
+  -I{} \
+    bash -c "ls -lth {}.binpack || ls -lth {}.tar || \
+             (echo Downloading {}.tar ... && \
+               wget -q $DATASET_URL/{}.tar || \
+                 (echo Failed to download {}.tar ; rm {}.tar ))"
 
 # clean up files from download errors (ie. files that don't exist)
 find $download_dir -name "*.tar" -size 0 -delete
