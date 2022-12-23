7a2bdb07a141ffe07c0f7b4e5fed4cb30a02edd8
Linmiao Xu <linmiao.xu@gmail.com>
2022-12-23 10:28:29 -0500

    Fix downloading tablebases properly

--- tb7/tablebase_dl.sh
+++ tb7/tablebase_dl.sh
@@ -1,119 +1,59 @@
 #!/bin/bash
 
-dl_dir1="/dev/shm"
+dl_dir1="~/data/syzygy/7p"
 dl_dir2="~/data/syzygy/7p"
 
 TB_URL="https://tablebase.lichess.ovh/tables/standard/7/"
 
 tablebases_to_download=(
-KBBBvKQP
-KBBBvKRR
-KBBNvKQP
-KBBNvKRR
-KBBPPvKQ
-KBBPvKBB
-KBBPvKBN
-KBBPvKNN
-KBBPvKRB
-KBBPvKRN
-KBNNvKQP
-KBNNvKRB
-KBNNvKRR
-KBNPPvKQ
-KBNPvKBB
-KBNPvKBN
-KBNPvKNN
-KBNPvKRB
-KBNPvKRN
-KBPPPvKR
-KBPPvKBB
-KBPPvKBN
-KBPPvKBP
-KBPPvKNN
-KBPPvKNP
-KBPPvKRP
-KNNNvKQP
-KNNNvKRB
-KNNNvKRN
-KNNNvKRR
-KNNPPvKQ
-KNNPvKBB
-KNNPvKBN
-KNNPvKNN
-KNNPvKRB
-KNNPvKRN
-KNNPvKRP
-KNPPPvKR
-KNPPvKBN
-KNPPvKBP
-KNPPvKNN
-KNPPvKNP
-KNPPvKRP
-KPPPPvKB
-KPPPPvKN
-KPPPPvKR
-KPPPvKBP
-KPPPvKNP
-KPPPvKPP
-KQBPvKQB
-KQBPvKQN
-KQBPvKQR
-KQNNvKQR
-KQNPvKQB
-KQNPvKQN
-KQNPvKQR
-KQPPvKQB
-KQPPvKQN
-KQPPvKQP
-KQPPvKRR
-KQQPvKQQ
-KQRBvKQQ
-KQRNvKQQ
-KQRPvKQR
-KQRRvKQQ
-KRBBvKQB
-KRBBvKQN
-KRBNvKQB
-KRBNvKQN
-KRBPPvKQ
-KRBPvKQP
-KRBPvKRB
-KRBPvKRN
-KRBPvKRR
-KRNNvKQB
-KRNNvKQN
-KRNNvKQP
-KRNNvKRR
-KRNPPvKQ
-KRNPvKQP
-KRNPvKRB
-KRNPvKRN
-KRNPvKRR
-KRPPPvKQ
-KRPPvKBB
-KRPPvKBN
-KRPPvKNN
-KRPPvKRB
-KRPPvKRN
-KRPPvKRP
-KRRBvKQB
-KRRBvKQN
-KRRBvKQR
-KRRNvKQB
-KRRNvKQN
-KRRNvKQR
-KRRPvKQB
-KRRPvKQN
-KRRPvKQP
-KRRPvKRR
-KRRRvKQR
+	KBBNvKQP
+	KBBNvKRR
+	KBBPPvKQ
+	KBBPvKRB
+	KBBPvKRN
+	KBNNvKQP
+	KBNNvKRR
+	KBNPPvKQ
+	KBNPvKBB
+	KBNPvKRB
+	KBNPvKRN
+	KBPPvKRP
+	KNNPPvKQ
+	KNNPvKBB
+	KNNPvKBN
+	KNNPvKRN
+	KNPPvKBP
+	KNPPvKRP
+	KPPPPvKB
+	KPPPPvKN
+	KQBPvKQR
+	KQNPvKQB
+	KQNPvKQR
+	KQRBvKQQ
+	KQRNvKQQ
+	KRBBvKQB
+	KRBBvKQN
+	KRBNvKQB
+	KRBNvKQN
+	KRBPvKQP
+	KRBPvKRR
+	KRNNvKQN
+	KRNPvKQP
+	KRNPvKRB
+	KRNPvKRR
+	KRPPvKBB
+	KRPPvKBN
+	KRPPvKNN
+	KRRBvKQR
+	KRRNvKQB
+	KRRNvKQR
 )
 
 function download_tb7_data() {
   pieces=$1  # KRBPvKQP, etc.
   for suffix in {rtbw,rtbz}; do
     tb_filename=$pieces.$suffix
-    if [ -f $dl_dir1/$tb_filename ] || [ -f $dl_dir2/$tb_filename ]; then
+    if [ -f $tb_filename ] || [ -f $tb_filename ]; then
       continue
     fi
     if echo $pieces | grep -E 'vK$' > /dev/null; then
@@ -129,7 +69,8 @@ function download_tb7_data() {
       subtype=${subtype}_pawnless
     fi
     echo $tb_filename not found. Downloading ${subtype} ...
-    # wget $TB_URL/$tb_filename -O $dl_dir1/$tb_filename
+    echo $TB_URL/$subtype/$tb_filename
+    wget $TB_URL/$subtype/$tb_filename # -O $dl_dir1/$tb_filename
   done
 }
 
